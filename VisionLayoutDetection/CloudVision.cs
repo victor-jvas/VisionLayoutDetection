db62dd121f17e7248008aa0cbd0718db03fde17f
jVictor <victor.jvas1@gmail.com>
2021-03-02 22:21:24 -0300

    Add project files.

---
+++ VisionLayoutDetection/CloudVision.cs
@@ -0,0 +1,78 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VisionLayoutDetection;
+
+namespace CloudVision
+{
+    public class Vision
+    {
+
+        public static string PrintStrings(string path, string expectStrings)
+        {
+            //var path = "D:\\Dev\\VisionTest\\IKGAME-897.png";
+            // Instantiates a client
+            //fvar client = ImageAnnotatorClient.Create();
+            // Load the image file into memory
+            //var image = Image.FromFile("D:\\Dev\\VisionTest\\IKGAME-897.png");
+            // Performs text detection on the image file
+            //var response = client.DetectDocumentText(image);
+
+            var response = ProcessImage(path);
+
+            List<string> paraList = new List<string> { };
+
+
+            foreach (Page pages in response.Pages)
+            {
+                foreach (Block blocks in pages.Blocks)
+                {
+                    foreach (Paragraph para in blocks.Paragraphs)
+                    {
+                        string sentence = "";
+                        foreach (Word words in para.Words)
+                        {
+                            foreach (Symbol sym in words.Symbols)
+                            {
+                                sentence += sym.Text;
+                            }
+                            sentence += " ";
+                        }
+                        paraList.Add(sentence);
+                    }
+                }
+            }
+            var expected = "Configurações";
+            //Console.WriteLine(MatchString(paraList, expected));
+            //DisplayResult(MatchString(paraList, expected));
+            return MatchString(paraList, expected);
+
+        }
+
+        public static string MatchString(List<string> text, string expected)
+        {
+            var match = text.Any(stringToCheck => string.Equals(stringToCheck.Trim(), expected));
+
+            if (!match)
+            {
+                var aux = "Erro na String: \"" + expected + "\"";
+                //DisplayResult(aux);
+                //Console.WriteLine("Erro na String: \"" + expected + "\"");
+                return aux;
+            }
+                
+            return "No Error";
+        }
+
+        public static TextAnnotation ProcessImage(string path)
+        {
+            var client = ImageAnnotatorClient.Create();
+
+            var image = Image.FromFile(path);
+
+            return client.DetectDocumentText(image);
+
+        }
+    }
+}
---
+++ VisionLayoutDetection/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CloudVision;
+
+namespace VisionLayoutDetection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string Result
+        {
+            get { return ResultTextBlock.Text; }
+            set { ResultTextBlock.Text = value; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FileSelect(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                // Set filter for file extension and default file extension 
+                DefaultExt = ".png",
+                Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif"
+            };
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                FileTextBox.Text = filename;
+            }
+        }
+
+        private void StringSelect(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                StringTextBox.Text = filename;
+            }
+        }
+
+        private void CallVision(object sender, RoutedEventArgs e)
+        {
+            var result = Vision.PrintStrings(FileTextBox.Text, StringTextBox.Text);
+            ResultTextBlock.Text = result;
+        }
+
+        
+    }
+}
